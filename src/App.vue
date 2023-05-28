81545a38cb40eb7f4f7bb8f7eeb1a9a507c78185
Sergej Garkusha <Tenebrann@googlemail.com>
2023-05-28 17:26:18 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
+<script>
+import ProductList from './components/ProductList.vue'
+import axios from './api/api'
+
+export default {
+  data() {
+    return {
+      products: []
+    }
+  },
+
+  mounted() {
+    axios.get(`products`).then((res) => (this.products = res.data))
+  }
+}
+</script>
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import MyHeader from './components/MyHeader.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <my-header></my-header>
+  <product-list :products="products"></product-list>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="products">
+    <product-item
+      v-for="list in props.products"
+      :key="list.id"
+      :title="list.title"
+      :description="list.description"
+      :image="list.image"
+      :price="list.price"
+      :rating="list.rating"
+    ></product-item>
+  </ul>
+</template>
+
+<script setup>
+import ProductItem from './ProductItem.vue'
+const props = defineProps(['products'])
+</script>
+
+<style lang="scss" scoped>
+.products {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  margin-top: 120px;
+}
+</style>
